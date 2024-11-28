d1e73eb548ffefa76e146c2b0539c81e324269b1
xavi276310 <xavi276310@gmail.com>
2024-11-28 17:41:40 +0800

    Initial commit

--- chroma-demo-reop.py
+++ chroma-demo-reop.py
@@ -5,13 +5,13 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # 1. 读取 CSV 数据
-data = pd.read_csv("/Users/mac/Downloads/jobs_extended_prod__date.csv")  # 替换为你的 CSV 文件路径
+data = pd.read_csv("/Users/mac/Downloads/jobs.csv")  # 替换为你的 CSV 文件路径
 
 # 2. 使用 SentenceTransformer 模型加载
 model = SentenceTransformer('all-MiniLM-L6-v2')  # 可以根据需要选择不同的模型
 
 # 3. 初始化 Chroma 持久化客户端，并指定存储路径
-chroma_client = chromadb.PersistentClient(path="/Users/mac/PycharmProjects/job_extended_20241111")  # 指定你的持久化存储路径
+chroma_client = chromadb.PersistentClient(path="/Users/mac/PycharmProjects/Resume-optimization-20241117")  # 指定你的持久化存储路径
 collection = chroma_client.create_collection(name="job_collection")  # 创建集合
 
 cnt = 0
@@ -22,11 +22,10 @@ for index, row in data.iterrows():
     cnt += 1
 
     job_title = row['job_title']
-    job_country = row['job_country']
-    job_location = row['job_location']
+    job_description = row['job_description']
 
     # 创建文档内容
-    document = f"Job Title: {job_title}, Country: {job_country}, Location: {job_location}"
+    document = f"Job Title: {job_title}, job_description: {job_description}"
 
     # 生成嵌入向量
     embedding = model.encode(document)
